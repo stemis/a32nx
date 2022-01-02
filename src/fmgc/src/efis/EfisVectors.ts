f4b8d5e4252204a1e1102c75f83d77492b302c36
Andreas Guther <andreas@guther.family>
2022-01-02 13:00:09 +0100

    fix: detect armed NAV correctly

--- src/fmgc/src/efis/EfisVectors.ts
+++ src/fmgc/src/efis/EfisVectors.ts
@@ -67,7 +67,7 @@ export class EfisVectors {
 
         const engagedLateralMode = SimVar.GetSimVarValue('L:A32NX_FMA_LATERAL_MODE', 'Number') as LateralMode;
         const armedLateralMode = SimVar.GetSimVarValue('L:A32NX_FMA_LATERAL_ARMED', 'Enum');
-        const navArmed = armedLateralMode !== 0;
+        const navArmed = (armedLateralMode & 1) === 1;
 
         const transmitActive = engagedLateralMode === LateralMode.NAV || engagedLateralMode === LateralMode.LOC_CPT || engagedLateralMode === LateralMode.LOC_TRACK || navArmed;
         const clearActive = !transmitActive && this.currentActiveVectors.length > 0;
